25246f39e06a4ca482c913f4e95c473da77a0aa7
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2025-07-23 20:24:19 +0700

    Add functionality to the buttons

--- main.js
+++ main.js
@@ -1,3 +1,117 @@
+let firstOperand = '';
+let currentOperator = '';
+let secondOperand = '';
+
+const display = document.querySelector('.display');
+let lastButtonId = '';
+
+document.querySelector(".button-container").addEventListener('click', handleButtonClick);
+
+function handleButtonClick(e) {
+    let buttonId = e.target.id;
+
+    if (!buttonId) {
+        return;
+    }
+
+    if (!Number.isNaN(+buttonId)) {
+        if (display.textContent == '0' || ['+', '-', '*', '/', '='].includes(lastButtonId)) {
+            display.textContent = buttonId;
+            shouldResetDisplay = false;
+        }
+        else {
+            display.textContent += buttonId;
+        }
+
+        if (lastButtonId == '=') {
+            clearState();
+        }
+        
+        if (currentOperator == '' ) {
+            firstOperand = display.textContent;
+        }
+        else {
+            secondOperand = display.textContent;
+        }
+    }
+    else if (buttonId == 'c') {
+        clearState();
+        display.textContent = '0';
+    }
+    else if (buttonId == '=') {
+        if (['+', '-', '*', '/'].includes(lastButtonId)) {
+            secondOperand = firstOperand;
+        }
+
+        if (currentOperator != '') {
+            evaluateExpression();
+        }
+    }
+    else {
+        if (secondOperand != '') {
+            evaluateExpression();
+        }
+
+        currentOperator = buttonId;
+    }
+
+    lastButtonId = buttonId;
+
+    console.log("firstOperand: " + firstOperand);
+    console.log("currentOperator: " + currentOperator);
+    console.log("secondOperand: " + secondOperand);
+    console.log("lastButtonId: " + lastButtonId);
+}
+
+function evaluateExpression() {
+    let result = operate(firstOperand, secondOperand, currentOperator);
+
+    if (result.toString().length > 7) {
+        result = parseFloat(result).toExponential(6);
+    }
+
+    display.textContent = result;
+
+    if (result == Infinity) {
+        clearState();
+        return;
+    }
+
+    firstOperand = display.textContent;
+}
+
+function clearState() {
+    firstOperand = '';
+    currentOperator = '';
+    secondOperand = '';
+}
+
+function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+ 
+    let result = 0;
+
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
+
 function add(a, b) {
     return a + b;
 }
